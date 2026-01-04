956055af9f71b1e8a5a7264d3e6321cb8b3d5175
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-01-04 21:23:43 +0200

    Implement front and back insertion for LinkedList

--- linkedList/linkedList.h
+++ linkedList/linkedList.h
@@ -13,8 +13,47 @@ namespace ds{
             size_t count;
         public:
             LinkedList();
+            bool isEmpty() const;
+            void insertFront(const Type& item);
+            void insertBack(const Type& item);
    };
 }
 
+
 template <class Type>
 ds::LinkedList<Type>::LinkedList():first(nullptr), last(nullptr), count(0){}
+
+template <class Type>
+bool ds::LinkedList<Type>::isEmpty() const{
+    return count == 0;
+}
+
+template <class Type>
+void ds::LinkedList<Type>::insertFront(const Type& item){
+    Node* newNode = new Node;
+    newNode->data = item;
+    if(isEmpty()) {
+        newNode->next = nullptr;
+        first = last = newNode;
+    }
+    else{
+        newNode->next = first;
+        first = newNode;
+    }
+    count++;
+}
+
+template <class Type>
+void ds::LinkedList<Type>::insertBack(const Type& item){
+    Node* newNode = new Node;
+    newNode->data = item;
+    newNode->next = nullptr;
+    if(isEmpty()) {
+        first = last = newNode;
+    }
+    else{
+        last->next = newNode;
+        last = newNode;
+    }
+    count++;
+}
