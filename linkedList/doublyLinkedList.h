b84fff56ce0270fd04ac624687ec81ab366285b1
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-01-09 16:51:02 +0200

    Add basic state queries and insertion operations to DoublyLinkedList

--- linkedList/doublyLinkedList.h
+++ linkedList/doublyLinkedList.h
@@ -12,9 +12,44 @@ namespace ds{
             size_t count;
         public:
             DoublyLinkedList();
+            bool isEmpty() const;
+            void insertFront(const Type& item);
+            void insertBack(const Type& item);
     };
 }
 
 
 template <class Type>
 ds::DoublyLinkedList<Type>::DoublyLinkedList():first(nullptr), last(nullptr), count(0){}
+
+template <class Type>
+bool ds::DoublyLinkedList<Type>::isEmpty() const{
+    return count == 0;
+}
+
+template <class Type>
+void ds::DoublyLinkedList<Type>::insertFront(const Type& item){
+    Node* newNode = new Node{item, nullptr, nullptr};
+    if(isEmpty()) {
+        first = last = newNode;
+    }
+    else{
+        newNode->next = first;
+        first->prev = newNode;
+        first = newNode;
+    }
+    count++;
+}
+
+template <class Type>
+void ds::DoublyLinkedList<Type>::insertBack(const Type& item){
+    if(isEmpty()){
+        insertFront(item);
+    }
+    else{
+        Node* newNode = new Node{item, nullptr, last};
+        last->next = newNode;
+        last = newNode;
+        count++;
+    }
+}
