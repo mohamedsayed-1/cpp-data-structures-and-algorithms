18457b85286ae87c929973d78f4f1ce76a329710
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-01-22 15:10:46 +0200

    Implement Rule of Three for array-based queue

--- queue/queueArrayBased.h
+++ queue/queueArrayBased.h
@@ -7,12 +7,48 @@ namespace ds{
             size_t length;
         public:
             queue(int size = 100);
+            ~queue();
+            queue(const queue<Type>& other);
+            queue& operator = (const queue<Type>& other);
         };
 }
+
 
 template <class Type>
 ds::queue<Type>::queue(int size):length(0){
     if(size <= 0) maxSize = 100;
     else maxSize = size;
     items = new Type[maxSize];
+}
+
+template <class Type>
+ds::queue<Type>::~queue(){
+    delete[] items;
+}
+
+template <class Type>
+ds::queue<Type>::queue(const queue<Type>& other){
+    Type* newArr = new Type[other.maxSize];
+    for (size_t i = 0; i < other.length; i++)
+    {
+        newArr[i] = other.items[i];
+    }
+    maxSize = other.maxSize;
+    length = other.length;
+    items = newArr;
+}
+
+template <class Type>
+ds::queue<Type>& ds::queue<Type>::operator = (const queue<Type>& other){
+    if(this == &other) return *this;
+    Type* newArr = new Type[other.maxSize];
+    for (size_t i = 0; i < other.length; i++)
+    {
+        newArr[i] = other.items[i];
+    }
+    delete[] items;
+    maxSize = other.maxSize;
+    length = other.length;
+    items = newArr;
+    return *this;
 }
