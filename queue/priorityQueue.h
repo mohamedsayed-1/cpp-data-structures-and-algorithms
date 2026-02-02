9d3560c8be911abc9c7d422ebf56d205393a6e96
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-02-02 21:59:49 +0200

    Implement sorted enqueue operation for PriorityQueue

--- queue/priorityQueue.h
+++ queue/priorityQueue.h
@@ -13,8 +13,36 @@ namespace ds{
             size_t count;
         public:
             PriorityQueue();
+            void enqueue(const Type& item);
     };
 }
 
 template <class Type>
 ds::PriorityQueue<Type>::PriorityQueue():first(nullptr), last(nullptr), count(0){}
+
+template <class Type>
+void ds::PriorityQueue<Type>::enqueue(const Type& item){
+    Node* newNode = new Node{item, nullptr};
+    if(isEmpty()) {
+        first = last = newNode;
+    }
+    else{
+        if(item >= first->data){
+            newNode->next = first;
+            first = newNode;
+        }
+        else if(item <= last->data){
+            last->next = newNode;
+            last = newNode;
+        }
+        else{
+            Node* curr = first;
+            while(curr->next != nullptr && item < curr->next->data){
+                curr = curr->next;
+            }
+            newNode->next = curr->next;
+            curr->next = newNode;
+        }
+    }
+    count++;
+}
