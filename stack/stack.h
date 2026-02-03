542064c799d3af97e7974e50d9ffc08b3bb36d29
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-02-03 06:43:52 +0200

    Implement Rule of Three for stack

--- stack/stack.h
+++ stack/stack.h
@@ -11,10 +11,45 @@ namespace ds{
             size_t length;
         public:
             Stack(int size = 100);
+            ~Stack();
+            Stack(const Stack<Type>& other);
+            Stack& operator = (const Stack<Type>& other);
     };
 }
 
 template <class Type>
 ds::Stack<Type>::Stack(int size):length(0), maxSize(size <= 0 ? 100 : size){
     items = new Type[maxSize];
+}
+
+template <class Type>
+ds::Stack<Type>::~Stack(){
+    delete[] items;
+}
+
+template <class Type>
+ds::Stack<Type>::Stack(const Stack<Type>& other){
+    Type* newArr = new Type[other.maxSize];
+    for (size_t i = 0; i < other.length; i++)
+    {
+        newArr[i] = other.items[i];
+    }
+    maxSize = other.maxSize;
+    length = other.length;
+    items = newArr;
+}
+
+template <class Type>
+ds::Stack<Type>& ds::Stack<Type>::operator = (const Stack<Type>& other){
+    if(this == &other) return *this;
+    Type* newArr = new Type[other.maxSize];
+    for (size_t i = 0; i < other.length; i++)
+    {
+        newArr[i] = other.items[i];
+    }
+    delete[] items;
+    maxSize = other.maxSize;
+    length = other.length;
+    items = newArr;
+    return *this;
 }
