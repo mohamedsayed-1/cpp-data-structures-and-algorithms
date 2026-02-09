448f5f04f904b3a9220d08ee847ae4615fb5146c
mohamedsyd <mohamedahmedsyd@gmail.com>
2026-02-09 11:26:26 +0200

    feat: add recursive insertion and search logic

--- BST/BST.h
+++ BST/BST.h
@@ -12,10 +12,59 @@ namespace ds{
                 };
             Node* root;
             size_t count;
+
+            Node* insert(Type item, Node* tree);
+            Node* search(Type item, Node* tree) const;
+
         public:
             BST();
+            void insert(Type item);
+            bool search(Type item) const;
+            size_t size() const;
+            bool isEmpty() const;
     };
 }
 
 template <class Type>
 ds::BST<Type>::BST():root(nullptr), count(0){}
+
+template <class Type>
+typename ds::BST<Type>::Node* ds::BST<Type>::insert(Type item, Node *tree){
+    if (tree == nullptr) {
+        Node* newNode = new Node(item); 
+        count++;
+        return newNode;
+    }
+    if (item < tree->data) tree->left = insert(item, tree->left);
+    if (item > tree->data) tree->right = insert(item, tree->right);
+    return tree;
+}
+
+template <class Type>
+void ds::BST<Type>::insert(Type item){
+    root = insert(item, root);
+}
+
+template <class Type>
+typename ds::BST<Type>::Node* ds::BST<Type>::search(Type item, Node *tree) const{
+    if (tree == nullptr) return nullptr;
+    if (item < tree->data) return search(item, tree->left);
+    if (item > tree->data) return search(item, tree->right);
+    if (item == tree->data) return tree;
+    return nullptr;
+}
+
+template <class Type>
+bool ds::BST<Type>::search(Type item)const{
+    return search(item, root) != nullptr;
+}
+
+template <class Type>
+size_t ds::BST<Type>::size() const{
+    return count;
+}
+
+template <class Type>
+bool ds::BST<Type>::isEmpty() const{
+    return count == 0;
+}
